76d6a8dca2c1f035fba0df9a36f5ffb1bcca0d1f
MacBook <macbook@MacBook-Pro-de-MacBook.local>
2025-12-19 11:30:07 +0100

    first commit

---
+++ src/main/java/so/stay/orchestrator/stayorchestrator/StayOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package so.stay.orchestrator.stayorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayOrchestratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/so/stay/orchestrator/stayorchestrator/StayOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package so.stay.orchestrator.stayorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayOrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
